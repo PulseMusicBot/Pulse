a2e5e42435adbed4ae57b4a24bc12df829192177
WesternPine <tyler@westernpine.dev>
2022-02-11 01:30:06 -0500

    Added forgotten shuffle command.
    Up Next:
         - Management/Premium status (Reuse old)

--- src/main/java/dev/westernpine/pulse/interactions/command/CommandManager.java
+++ src/main/java/dev/westernpine/pulse/interactions/command/CommandManager.java
@@ -69,6 +69,7 @@ public class CommandManager {
                         new Move(),
                         new Remove(),
                         new Repeat(),
+                        new Shuffle(),
                         new SkipTo()
                 )
                 .sorted()
